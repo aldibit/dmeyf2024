2a705c85c5f7cd8f8eff16e1bfe4e35306428733
aldibit <167156659+aldibit@users.noreply.github.com>
2024-11-23 18:31:55 -0300

    Update 990_workflow_orden227_SEMI2311.r

--- src/workflows/990_workflow_orden227_SEMI2311.r
+++ src/workflows/990_workflow_orden227_SEMI2311.r
@@ -101,7 +101,7 @@ FEintra_manual_base <- function( pinputexps )
   if( -1 == (param_local <- exp_init())$resultado ) return( 0 ) # linea fija
 
 
-  param_local$meta$script <- "/src/wf-etapas/z1301_FE_intrames_manual.r"
+  param_local$meta$script <- "/src/wf-etapas/1301_FE_intrames_manual.r"
 
   param_local$semilla <- NULL  # no usa semilla, es deterministico
 
