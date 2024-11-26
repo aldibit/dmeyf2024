240484e50746dc223ebd265b8c4e8b451ee5affa
aldibit <167156659+aldibit@users.noreply.github.com>
2024-11-26 01:38:06 -0300

    Update and rename C3_990_workflow_orden227_SEMI2311.r to C3_990_workflow_orden227_SEMI755737.r

--- src/workflows/C3_990_workflow_orden227_SEMI755737.r
+++ src/workflows/C3_990_workflow_orden227_SEMI755737.r
@@ -19,7 +19,7 @@ envg$EXPENV$repo_dir <- "~/dmeyf2024/"
 envg$EXPENV$datasets_dir <- "~/buckets/b1/datasets/"
 envg$EXPENV$messenger <- "~/install/zulip_enviar.sh"
 
-envg$EXPENV$semilla_primigenia <- 102191
+envg$EXPENV$semilla_primigenia <- 755737
 
 # leo el unico parametro del script
 args <- commandArgs(trailingOnly=TRUE)
@@ -151,7 +151,7 @@ FEhist_base <- function( pinputexps)
   param_local$Tendencias1$ratiomax <- FALSE
 
   # no me engraso las manos con las tendencias de segundo orden
-  param_local$Tendencias2$run <- FALSE
+  param_local$Tendencias2$run <- TRUE
   param_local$Tendencias2$ventana <- 12
   param_local$Tendencias2$tendencia <- TRUE
   param_local$Tendencias2$minimo <- FALSE
@@ -263,18 +263,23 @@ TS_strategy_base8 <- function( pinputexps )
 
   param_local$meta$script <- "/src/wf-etapas/z2101_TS_training_strategy.r"
 
-  param_local$future <- c(202108)
+  param_local$future <- c(202109)
 
   param_local$final_train$undersampling <- 1.0
   param_local$final_train$clase_minoritaria <- c( "BAJA+1", "BAJA+2")
-  param_local$final_train$training <- c(202106, 202105, 202104,
-    202103, 202102, 202101)
+  param_local$final_train$training <- c(202106, 202105, 202104, 202103, 202102, 202101,
+                                        202012, 202011, 202010, 202009, 202008, 202007,
+                                        202006, 202005, 202004, 202003, 202002, 202001,
+                                        201912, 201911, 201910, 201909, 201908, 201907,
+                                        201906, 201905, 201904, 201903, 201902, 201901)
 
-
-  param_local$train$training <- c(202104, 202103, 202102,
-    202101, 202012, 202011)
-  param_local$train$validation <- c(202105)
-  param_local$train$testing <- c(202106)
+  param_local$train$training <- c(202106, 202105, 202104, 202103, 202102, 202101,
+                                        202012, 202011, 202010, 202009, 202008, 202007,
+                                        202006, 202005, 202004, 202003, 202002, 202001,
+                                        201912, 201911, 201910, 201909, 201908, 201907,
+                                        201906, 201905, 201904, 201903, 201902, 201901)
+  param_local$train$validation <- c(202106)
+  param_local$train$testing <- c(202107)
 
 
   # Atencion  0.2  de  undersampling de la clase mayoritaria,  los CONTINUA
@@ -294,40 +299,37 @@ TS_strategy_base8 <- function( pinputexps )
 
   param_local$meta$script <- "/src/wf-etapas/z2101_TS_training_strategy.r"
 
-  param_local$future <- c(202108)
+  param_local$future <- c(202109)
 
   param_local$final_train$undersampling <- 0.02
   param_local$final_train$clase_minoritaria <- c( "BAJA+1", "BAJA+2")
-  param_local$final_train$training <- c(
-    202106, 202105, 
-    #202104, 202103,
-    202102, 202101, 
-    202012, 202011, 202010, 202009, 202008, 202007, 
-    # 202006  Excluyo por variables rotas
-    202005, 202004, 202003, 202002, 202001,
-    201912, 201911,
-    # 201910 Excluyo por variables rotas
-    201909, 201908, 201907, 201906,
-    # 201905  Excluyo por variables rotas
-    201904, 201903
-  )
+  param_local$final_train$training <- c(202106, 202105, 
+                                        #202104, 202103,
+                                        202102, 202101,
+                                        202012, 202011, 202010, 202009, 202008, 202007,
+                                        #202006, 
+                                        202005, 202004, 202003, 202002, 202001,
+                                        201912, 201911,
+                                        #201910, 
+                                        201909, 201908, 201907, 201906, 
+                                        #201905, 
+                                        201904, 201903, 201902, 201901)
 
 
   param_local$train$testing <- c(202106)
   param_local$train$validation <- c(202105)
 
-  param_local$train$training <- c(
-    # 202104, 202103, 
-    202102, 202101, 
-    202012, 202011, 202010, 202009, 202008, 202007, 
-    # 202006  Excluyo por variables rotas
-    202005, 202004, 202003, 202002, 202001,
-    201912, 201911,
-    # 201910 Excluyo por variables rotas
-    201909, 201908, 201907, 201906,
-    # 201905  Excluyo por variables rotas
-    201904, 201903
-  )
+  param_local$train$training <- c(202106, 202105, 
+                                        #202104, 202103,
+                                        202102, 202101,
+                                        202012, 202011, 202010, 202009, 202008, 202007,
+                                        #202006, 
+                                        202005, 202004, 202003, 202002, 202001,
+                                        201912, 201911,
+                                        #201910, 
+                                        201909, 201908, 201907, 201906, 
+                                        #201905, 
+                                        201904, 201903, 201902, 201901)
 
 
   # Atencion  0.2  de  undersampling de la clase mayoritaria,  los CONTINUA
@@ -484,7 +486,7 @@ wf_SEMI_ago_orden227 <- function( pnombrewf )
   param_local <- exp_wf_init( pnombrewf ) # linea fija
 
   # Etapa especificacion dataset de la Segunda Competencia Kaggle
-  DT_incorporar_dataset( "~/buckets/b1/datasets/competencia_02.csv.gz")
+  DT_incorporar_dataset( "~/buckets/b1/datasets/competencia_03.csv.gz")
 
   CA_catastrophe_base( metodo="MachineLearning")
   FEintra_manual_base()
@@ -520,6 +522,6 @@ wf_SEMI_ago_orden227 <- function( pnombrewf )
 # Aqui comienza el programa
 
 # llamo al workflow con future = 202108
-wf_SEMI_ago_orden227()
+wf_SEMI_ago_orden227_SEMILLA755737()
 
 
